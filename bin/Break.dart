f678dea4d0d004bc213b7c29ec50525c298b27f8
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-02-23 21:05:22 +0700

    commit heula

--- bin/Break.dart
+++ bin/Break.dart
@@ -1,29 +1,28 @@
-void main(){
-  /*
-  BREAK adalah perintah khusus yang dipakai
-   untuk memaksa sebuah perulangan berhenti
-   sebelum waktunya.
-   */
+void main() {
+    /*
+      Break adalah sebuah keyword yang dipakai untuk
+      memaksa sebuah perulangan berhenti sebelum waktunya.
+     */
 
-  var nilai = 1;
-  while(true){
-    print('Perulangan ke - $nilai');
-    nilai++;
-    if(nilai == 10){
-      break;
+    var nilai = 1;
+    while(true) {
+      print('Perulangan ke - $nilai');
+      nilai++;
+      if (nilai == 10) {
+        break;
+      }
     }
-  }
-  print('Akhir dari looping\n');
+    print('Akhir dari looping\n');
 
-//  membuat daftar penambahan angka
-/*
-    1 + 1 = 2
-    dst..
- */
-  for(var i = 1; i<=10; i++) {
-    print('$i + $i = ${i + i}');
-    if(i == 5){
-      break;
+//    membuat daftar penambahan angka
+      /*
+        1 + 1 = 2
+        dst...
+       */
+    for(var i = 1; i<=10; i++){
+      print('$i + $i = ${i+i}');
+      if(i == 5){
+        break;
+      }
     }
-  }
 }
--- bin/Continue.dart
+++ bin/Continue.dart
@@ -1,21 +1,20 @@
-void main(){
-
+void main() {
   /*
-  Continue digunakan untuk menghentikan
-  perulangan saat ini, lalu melanjutkan
-  ke perulangan selanjutnya
+    Keyword continue berfungsi untuk
+    mengentikan perulangan saat ini,
+    lalu melanjutkan ke perulangan selanjutnya
    */
-  for(var i =1; i<=100; i++ ){
-    if(i % 2 == 1){
-      continue;
+    for(var i = 1; i<=100; i++){
+      if(i % 2 == 1){
+        continue;
+      }
+      print('Perulangan Ganjil - $i');
     }
-    print('Perulangan Genap - $i');
-  }
 
-  for(var i = 1; i<=10; i++) {
-    if(i == 5){
-      continue;
+    for(var i = 1; i<=10; i++){
+      if(i == 5){
+        continue;
+      }
+      print('$i + $i = ${i+i}');
     }
-    print('$i + $i = ${i + i}');
-  }
 }
